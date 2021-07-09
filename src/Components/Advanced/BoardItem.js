064e721713fb4e0495e0dbbd31f3a3a38990a55b
Yunsun Jung <99yunsun@naver.com>
2021-07-09 10:05:43 +0900

    feat: add BoardItem

--- src/Components/Advanced/BoardItem.js
+++ src/Components/Advanced/BoardItem.js
@@ -1,9 +1,88 @@
 import React from 'react'
+import styled from 'styled-components'
+import CopyButton from '../Basic/CopyButton'
 
 export default function BoardItem() {
   return (
-    <div>
-      
-    </div>
+    <StyledBoardItem>
+      <Thumbnail />
+      <BoardInfo>
+        <BoardNameContainer>
+          <BoardName>호캉스 계획</BoardName>
+          <ButtonContainer>
+            <CopyButton />
+            <KebabButton>
+              <img src={process.env.PUBLIC_URL + '/assets/Kebab.svg'} />
+            </KebabButton>
+          </ButtonContainer>
+        </BoardNameContainer>
+      </BoardInfo>
+    </StyledBoardItem>
   )
 }
+
+const StyledBoardItem = styled.div`
+  width: 488px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(97, 131, 190, 0.3);
+  background: white;
+  padding: 0;
+  cursor: pointer;
+  transition: 0.3s ease;
+  display: flex;
+  flex-direction: row;
+  &:hover {
+    box-shadow: 0px 0px 10px rgba(97, 131, 190, 0.8);
+  }
+`
+
+const Thumbnail = styled.div`
+  width: 200px;
+  height: 100%;
+  border-radius: 10px 0 0 10px;
+  background-color: blue;
+`
+
+const BoardInfo = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+const BoardNameContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+const BoardName = styled.div`
+  font-size: 1.5em;
+  font-family: Pretendard;
+  font-weight: 700;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const KebabButton = styled.div`
+  width: 16px;
+  position: relative;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+  & svg {
+    fill: #BEBEBE;
+    stroke: white;
+    stroke-width: 0.2;
+    cursor: pointer;
+  }
+  &:hover svg {
+    fill: black;
+  }
+`
--- src/Views/AdvancedComponents.js
+++ src/Views/AdvancedComponents.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container, Row, Col, ListGroup } from "react-bootstrap";
 import Renderer from '../Components/Renderer';
 import LinkItem from '../Components/Advanced/LinkItem';
+import BoardItem from '../Components/Advanced/BoardItem';
 
 export default function AdvancedComponents() {
   return (
@@ -17,6 +18,9 @@ export default function AdvancedComponents() {
           <h1>LinkItem</h1>
           <p>보드 내부에 존재하는 각각의 링크들</p>
           <Renderer component={<LinkItem />} bg="pl-lightblue" noshadow />
+          <h1>BoardItem</h1>
+          <p>보드들ㅎㅎ</p>
+          <Renderer component={<BoardItem />} bg="pl-lightblue" noshadow />
         </Container>
       </Col>
     </Row>
